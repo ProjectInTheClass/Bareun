30afbf04781c404af6d7b8177b26a15acf4a8fb0
miiinnn23 <miiinnn23@users.noreply.github.com>
2021-01-06 00:13:18 +0900

    Edit Font&Category background

--- Bareun/Bareun/API.swift
+++ Bareun/Bareun/API.swift
@@ -11,10 +11,10 @@ class MENU {
     static let shared = MENU()
     
     var items:[MenuItem] = [
-        MenuItem(name: "나를 깨우는 명언", image: "menu.png"),
-        MenuItem(name: "많이 틀리는 맞춤법", image: "menu.png"),
-        MenuItem(name: "쓸모있는 영어 문장", image: "menu.png"),
-        MenuItem(name: "대학 슬로건", image: "menu.png"),
+        MenuItem(name: "나를 깨우는 명언", image: "category.png"),
+        MenuItem(name: "많이 틀리는 맞춤법", image: "category.png"),
+        MenuItem(name: "쓸모있는 영어 문장", image: "category.png"),
+        MenuItem(name: "대학 슬로건", image: "category.png"),
     ]
     
     func getMenuItems(completion: ([MenuItem]) -> Void) {
