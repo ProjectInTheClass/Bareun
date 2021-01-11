9f512ed8660d1e260c3cca9f3ec00674f8bc00cf
miiinnn23 <miiinnn23@users.noreply.github.com>
2021-01-11 22:07:15 +0900

    Add Drawing Option
    펜슬킷 import 하여 그림그리는 옵션을 추가하였습니다

--- Bareun/Bareun/DetailViewController.swift
+++ Bareun/Bareun/DetailViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import PencilKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
 
+    @IBOutlet weak var canvasView: PKCanvasView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var backgroundImg: UIImageView!
     var menu:MenuItem? = nil
+    
+    let canvasWidth: CGFloat = 828
+    let canvasOverscrollHeight:CGFloat = 500
+    
+    var drawing = PKDrawing()
+    var toolPicker: PKToolPicker!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +29,56 @@ class DetailViewController: UIViewController {
         if titleLabel.text == "쓸모있는 영어 문장" {
             backgroundImg.image = UIImage(named: "backgroundeng.png")
         }
+        
+        canvasView.delegate = self
+        canvasView.drawing = drawing
+        
+        canvasView.alwaysBounceVertical = true
+        canvasView.drawingPolicy = .anyInput
+        if #available(iOS 14.0, *) {
+            toolPicker = PKToolPicker()
+        } else {
+            let window = parent?.view.window
+            toolPicker = PKToolPicker.shared(for: window!)
+        }
+        
+        toolPicker.setVisible(true, forFirstResponder: canvasView)
+        toolPicker.addObserver(canvasView)
+        toolPicker.addObserver(self)
+//        updateLayout(for: toolPicker)
+        canvasView.becomeFirstResponder()
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let canvasScale = canvasView.bounds.width / canvasWidth
+        canvasView.minimumZoomScale = canvasScale
+        canvasView.maximumZoomScale = canvasScale
+        canvasView.zoomScale = canvasScale
+        
+        updateContentSizeForDrawing()
+        canvasView.contentOffset = CGPoint(x: 0, y: -canvasView.adjustedContentInset.top)
+    }
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
     }
     
+    func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+        updateContentSizeForDrawing()
+    }
+    
+    func updateContentSizeForDrawing() {
+        let drawing = canvasView.drawing
+        let contentHeight: CGFloat
+        
+        if !drawing.bounds.isNull {
+            contentHeight = max(canvasView.bounds.height, (drawing.bounds.maxY + self.canvasOverscrollHeight) * canvasView.zoomScale)
+        } else {
+            contentHeight = canvasView.bounds.height
+        }
+        
+        canvasView.contentSize = CGSize(width: canvasWidth * canvasView.zoomScale, height: contentHeight)
+    }
 
     /*
     // MARK: - Navigation
