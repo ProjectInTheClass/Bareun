960e96afb831bb23ce519aaf82ed098f8acc1bb9
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2021-01-16 11:35:06 +0900

    Add ImageSimilarityViewController

---
+++ Bareun/Bareun/ImageSimilarityViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ImageSimilarityViewController.swift
+//  Bareun
+//
+//  Created by YangYujin on 2021/01/16.
+//
+
+import UIKit
+
+class ImageSimilarityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
