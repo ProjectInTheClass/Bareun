7de8c0fc63588f242c4897a36ab900ef3847f531
Seyoung1220 <62585390+Seyoung1220@users.noreply.github.com>
2021-01-16 23:24:58 +0900

    Revert "Merge branch 'main' of https://github.com/ProjectInTheClass/Bareun into main"
    This reverts commit 787e245ed8c4a28174adf5bd97b096dfe9fed941, reversing
    changes made to 5e7326edcc0473b936929cd56640a62d38492b8b.

--- Bareun/Bareun/DetailViewController.swift
+++ Bareun/Bareun/DetailViewController.swift
@@ -72,7 +72,6 @@ class DetailViewController: UIViewController, PKCanvasViewDelegate, PKToolPicker
             print("error!")
         }
         countLabel.text = "\((imageIndex) + 1)/\(tempArray.count)"
-        Shared.shared.CurTextImage = tempArray[imageIndex]
         
         canvasView.delegate = self
         canvasView.drawing = drawing
@@ -152,11 +151,10 @@ class DetailViewController: UIViewController, PKCanvasViewDelegate, PKToolPicker
                     }
                     
                     self.textImage.image = UIImage(named: self.tempArray[self.imageIndex])
-                    Shared.shared.CurTextImage = self.tempArray[self.imageIndex]
                 }
             }
         }
-        
+        Shared.shared.CurTextImage = self.tempArray[self.imageIndex]
         
         if segue.identifier == "testImage" {
             let dvc = segue.destination as! ImageSimilarityViewController
@@ -242,7 +240,6 @@ class DetailViewController: UIViewController, PKCanvasViewDelegate, PKToolPicker
             countLabel.text = "\(imageIndex+1)/\(tempArray.count)"
         }
         textImage.image = UIImage(named: tempArray[imageIndex])
-        Shared.shared.CurTextImage = tempArray[imageIndex]
     }
     
     @IBAction func goToPreviousPage(_ sender: Any) {
@@ -255,7 +252,6 @@ class DetailViewController: UIViewController, PKCanvasViewDelegate, PKToolPicker
             countLabel.text = "\(imageIndex+1)/\(tempArray.count)"
         }
         textImage.image = UIImage(named: tempArray[imageIndex])
-        Shared.shared.CurTextImage = tempArray[imageIndex]
     }
     
     
