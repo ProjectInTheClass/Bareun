242162716bab60b8c91a9d49ff28aab1b27f98b2
miiinnn23 <miiinnn23@users.noreply.github.com>
2021-01-18 15:45:50 +0900

    Edit font license func

--- Bareun/Bareun/FontLicenseViewController.swift
+++ Bareun/Bareun/FontLicenseViewController.swift
@@ -14,6 +14,7 @@ class FontLicenseViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(licenseText)
         // Do any additional setup after loading the view.
 //        content.text = Shared.shared.fontLicense
 //        if let filepath = Bundle.main.path(forResource: "OFL-Pinyon", ofType: "txt") {
