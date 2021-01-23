e5a48a350603b0343c23bd4d709ab583fa9fd3d4
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2021-01-23 21:28:42 +0900

    Update Image Similarity

--- Bareun/Bareun/ImageSimilarityViewController.swift
+++ Bareun/Bareun/ImageSimilarityViewController.swift
@@ -42,16 +42,52 @@ class ImageSimilarityViewController: UIViewController {
         
     }
     
+    func TransperentImageToWhite(image: UIImage) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(image.size, false, image.scale)
+        let imageRect: CGRect = CGRect.init(0.0, 0.0, image.size.width, image.size.height)
+        let ctx: CGContext = UIGraphicsGetCurrentContext()!
+        // Draw a white background (for white mask)
+        ctx.setFillColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        ctx.fill(imageRect)
+        // Apply the source image's alpha
+        image.draw(in: imageRect, blendMode: .normal, alpha: 1.0)
+        let mask: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return mask
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         var modelImage: UIImage = UIImage(named:Shared.shared.CurTextImage ?? "c1_01_mj")!
         modelImage = modelImage.cropToRect(rect: CGRect.init(0.0,0.0,(modelImage.size.width),(modelImage.size.height)/1.5))!
+//        newImage = TransperentImageToWhite(image: newImage!)
+        guard let currentCGImage = newImage?.cgImage else { return }
+        let currentCIImage = CIImage(cgImage: currentCGImage)
+
+        let filter = CIFilter(name: "CIColorMonochrome")
+        filter?.setValue(currentCIImage, forKey: "inputImage")
+
+        // set a gray value for the tint color
+        filter?.setValue(CIColor(red: 0.58, green: 0.58, blue: 0.58), forKey: "inputColor")
+
+        filter?.setValue(1.0, forKey: "inputIntensity")
+        guard let outputImage = filter?.outputImage else { return }
+
+        let context = CIContext()
+
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let processedImage = UIImage(cgImage: cgimg)
+            newImage = processedImage
+//            print(processedImage.size)
+        }
+//        newImage = newImage?.maskWithColors(color: textColor)
         original.image = modelImage
         compare.image = newImage
-//        compare.image = UIImage(named: ")
+ß
         
         scoreLabel.isHidden = true
     }
 }
+
